5d8e810926d3d9278f183cf766eeac036a9ee330
vinkami <vinkami.krip@gmail.com>
2025-01-31 16:51:54 +0800

    Added requirement to pass filename to Main.kt to run

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,12 @@
 import vklang.Script
 import vklang.interpret.SymbolTable
 
-fun main() {
-    val script = Script("src/test.vk")
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        println("Usage: vklang <filename>")
+        return
+    }
+    val script = Script(args[0])
     val st = SymbolTable({ s: String -> println(s)})
     val err = script.interpret(st)
     if (err != null) {
